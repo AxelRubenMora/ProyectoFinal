d4360dd3d59730a013c44296270ded570d4598fb
Axel Ruben Mora Mendieta <axelrmora@gmail.com>
2022-09-18 11:16:12 -0700

    Primer_Comit

---
+++ Tarea_de_Curso/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import ui.Menu;
+
+public class Main {
+	public static void main(String[] args) {
+		Menu nenus= new Menu();
+		System.out.println("Bienvenido al Cajero Java una invencion de:\nAxel y Angel\nPorfavor Digite la opereacion que desee efectuar");
+		nenus.take_actions();
+	}
+
+}
